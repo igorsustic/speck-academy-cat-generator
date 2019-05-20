1e1ad2c93909ecf92c0d893dc79f696d8845b27b
Igor Sustic <igor.sustic@gmail.com>
2019-05-20 21:52:55 +0200

    Add script.js, API url, querryselectors

---
+++ script.js
@@ -0,0 +1,5 @@
+const catApiURL = 'https://api.thecatapi.com/v1/images/search';
+
+const btnGenerateCat = document.querySelector(".js-generate-cat");
+
+const resultsContainer = document.querySelector(".js-catz");
