8684c1a11c579c8010db04422eecde104dcfe3df
Igor Sustic <igor.sustic@gmail.com>
2019-05-20 21:55:09 +0200

    Add function with promise

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ const catApiURL = 'https://api.thecatapi.com/v1/images/search';
 const btnGenerateCat = document.querySelector(".js-generate-cat");
 
 const resultsContainer = document.querySelector(".js-catz");
+
+
+
+function fetchData (url) {
+    return fetch(catApiURL)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(response) { 
+            return response[0].url;
+        })
+    }
